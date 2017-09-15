f6d9b464dc755d492a328c5c413c9e289a54c87d
Daniel Rotchford <jimrockford45@gmail.com>
2017-09-15 20:13:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -40,20 +40,17 @@ console.log(string.toLowerCase())
 
 function sayHiToGrandma(string) {
 
-  if (string.toLowerCase() === string){
+  if (string.toLowerCase() === string) {
 
   return "I can't hear you!"
 }
 
-function sayHiToGrandma(string) {
 
   if (string.toUpperCase() === string) {
 
 return "YES INDEED!"
 
 }
-
-function sayHiToGrandma(string) {
 
   if (string === "I love you, Grandma.") {
 
